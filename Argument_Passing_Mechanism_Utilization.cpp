9cc3adb45f12184e2bf912690cec8880f43f931a
yangjoon03 <pythoncloud0115@gmail.com>
2024-12-13 13:04:59 +0900

    Update Argument_Passing_Mechanism_Utilization.cpp

--- Argument_Passing_Mechanism_Utilization.cpp
+++ Argument_Passing_Mechanism_Utilization.cpp
@@ -16,6 +16,11 @@ void call_by_reference::manage() {
 	Value_plus(a);
 	Value_plus(&aa);
 	cout << "a : " << a << " / aa : " << aa << endl;
+
+	int b = 10;	//동일한 코드로 2개의 변수 제어가 가능해짐.
+	int bb = 10;
+	Value_plus(&b);
+	Value_plus(&bb);
 }
 
 
