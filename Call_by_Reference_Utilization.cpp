a61906daee0964d45372a0c38c23eca028f9be1a
yangjoon03 <pythoncloud0115@gmail.com>
2024-12-13 12:54:40 +0900

    Create Call_by_Reference_Utilization.cpp

---
+++ Call_by_Reference_Utilization.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class call_by_reference {
+public:
+	void manage();
+	void Value_plus(int* n) { (*n)++; };
+	void Value_plus(int n) {  n++; };
+};
+
+void call_by_reference::manage() {
+	int a = 10;
+	int aa = 10;
+	cout << "a : " << a << " / aa : " << aa << endl;
+	Value_plus(&a);
+	Value_plus(a);
+	cout << "a : " << a << " / aa : " << aa << endl;
+}
+
+
+int main() {
+	call_by_reference R;
+	R.manage();
+
+	return 0;
+}
+
+
